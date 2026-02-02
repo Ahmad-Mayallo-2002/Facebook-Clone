d94f4ac464038325d3a19169dc0952dc06df28b4
Ahmad Mayallo <ahmadmayallo02@gmail.com>
2026-02-02 13:29:38 +0200

    Adding React Service and Resolver

--- server/src/utils/buildSchema.ts
+++ server/src/utils/buildSchema.ts
@@ -3,8 +3,9 @@ import { AuthResolver } from "../resolvers/auth.resolver";
 import { UserResolver } from "../resolvers/user.resolver";
 import { PostResolver } from "../resolvers/post.resolver";
 import { CommentResolver } from "../resolvers/comment.resolver";
+import { ReactResolver } from "../resolvers/react.resolver";
 import { ResolveTime } from "../middlewares/resolveTime.middleware";
 
-export const resolvers: NonEmptyArray<Function> = [UserResolver, AuthResolver, PostResolver, CommentResolver];
+export const resolvers: NonEmptyArray<Function> = [UserResolver, AuthResolver, PostResolver, CommentResolver, ReactResolver];
 
 export const globalMiddlewares = [ResolveTime];
